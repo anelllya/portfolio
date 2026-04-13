d8ce2a67439f4182bea437eebe456fd156ea32a0
Anelia <nellyes1@gmail.com>
2026-04-13 18:36:13 +0100

    yrs of exp variable calc update

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -11,3 +11,9 @@ window.addEventListener('DOMContentLoaded', updateYear);
 // Update the year when a new page is loaded
 var observer = new MutationObserver(updateYear);
 observer.observe(document.body, {childList: true});
+
+// Re-populate years-of-experience on every instant navigation (MkDocs Material SPA)
+document$.subscribe(function () {
+    var yrs = new Date().getFullYear() - 2009 + "+";
+    document.querySelectorAll(".tx-years-xp").forEach(function (el) { el.textContent = yrs; });
+});
